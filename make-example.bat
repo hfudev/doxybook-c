3c05e6d9c07b44795125c5dd1ae63f541fb7e91a
Fu Hanxi <fuhanxi@espressif.com>
2022-10-11 15:05:36 +0800

    example: use c project

--- make-example.bat
+++
@@ -1,7 +0,0 @@
-cd example
-doxygen Doxyfile
-cd ..
-python -m doxybook -t gitbook -i example/temp/xml -o example/gitbook/api -s example/gitbook/SUMMARY.md
-python -m doxybook -t vuepress -i example/temp/xml -o example/vuepress/api
-python -m doxybook -t docsify -i example/temp/xml -o example/docsify/api
-python -m doxybook -t mkdocs -i example/temp/xml -o example/mkdocs/docs/api
